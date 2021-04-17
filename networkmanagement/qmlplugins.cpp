f79b8f3feb03cc5a10cb8904e7b394c180284850
cutefishd <cutefishos@foxmail.com>
2021-04-18 03:03:21 +0800

    Improve network management

--- networkmanagement/qmlplugins.cpp
+++ networkmanagement/qmlplugins.cpp
@@ -1,5 +1,4 @@
 #include "qmlplugins.h"
-#include "networking.h"
 #include "networkmodel.h"
 #include "networkmodelitem.h"
 #include "appletproxymodel.h"
@@ -8,6 +7,10 @@
 #include "connectionicon.h"
 #include "network.h"
 #include "identitymodel.h"
+#include "handler.h"
+#include "enabledconnections.h"
+#include "enums.h"
+#include "wifisettings.h"
 
 #include <QQmlEngine>
 
@@ -16,11 +19,14 @@ void QmlPlugins::registerTypes(const char* uri)
     qmlRegisterUncreatableType<NetworkModelItem>(uri, 1, 0, "NetworkModelItem",
                                                 QLatin1String("Cannot instantiate NetworkModelItem"));
     qmlRegisterType<AppletProxyModel>(uri, 1, 0, "AppletProxyModel");
-    qmlRegisterType<Networking>(uri, 1, 0, "Networking");
     qmlRegisterType<NetworkModel>(uri, 1, 0, "NetworkModel");
     qmlRegisterType<TechnologyProxyModel>(uri, 1, 0, "TechnologyProxyModel");
     qmlRegisterType<WirelessItemSettings>(uri, 1, 0, "WirelessItemSettings");
     qmlRegisterType<ConnectionIcon>(uri, 1, 0, "ConnectionIcon");
     qmlRegisterType<Network>(uri, 1, 0, "Network");
     qmlRegisterType<IdentityModel>(uri, 1, 0, "IdentityModel");
+    qmlRegisterType<Handler>(uri, 1, 0, "Handler");
+    qmlRegisterType<EnabledConnections>(uri, 1, 0, "EnabledConnections");
+    qmlRegisterType<WifiSettings>(uri, 1, 0, "WifiSettings");
+    qmlRegisterUncreatableType<Enums>(uri, 1, 0, "Enums", "You cannot create Enums on yourself");
 }
